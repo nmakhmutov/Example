b22ed3db8e2dd287e173586fd159128a0680bc8e
Nail Makhmutov <makhmutov.nail@gmail.com>
2019-09-02 07:39:31 +0300

    Small_changes

--- src/Elwark.EventBus.Logging.EF/IIntegrationEventLogService.cs
+++ src/Elwark.EventBus.Logging.EF/IIntegrationEventLogService.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
 using System.Threading;
 using System.Threading.Tasks;
 using Elwark.EventBus.Abstractions;
---
+++ src/Elwark.EventBus.RabbitMQ/RabbitMqSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace Elwark.EventBus.RabbitMQ
+{
+    public class RabbitMqSettings
+    {
+        public RabbitMqSettings(string hostName, string username, string password, string virtualHost,
+            string exchange, string queueName, ushort retryCount, string connectionName)
+        {
+            HostName = hostName;
+            Username = username;
+            Password = password;
+            VirtualHost = virtualHost;
+            Exchange = exchange;
+            QueueName = queueName;
+            RetryCount = retryCount;
+            ConnectionName = connectionName;
+        }
+
+        public string HostName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string VirtualHost { get; set; }
+        public string Exchange { get; set; }
+        public string QueueName { get; set; }
+        public string ConnectionName { get; set; }
+        public ushort RetryCount { get; set; }
+    }
+}
