0bc8a45353f4c180fab2ceacc23da0d0e7e755f0
Nail Makhmutov <makhmutov.nail@gmail.com>
2019-12-01 09:13:44 +0300

    Added get amqp connection

--- src/Elwark.EventBus.RabbitMQ/RabbitMqSettings.cs
+++ src/Elwark.EventBus.RabbitMQ/RabbitMqSettings.cs
@@ -1,11 +1,15 @@
-﻿namespace Elwark.EventBus.RabbitMQ
+﻿using System.Text;
+
+namespace Elwark.EventBus.RabbitMQ
 {
     public class RabbitMqSettings
     {
-        public RabbitMqSettings(){}
-        
+        public RabbitMqSettings()
+        {
+        }
+
         public RabbitMqSettings(string hostName, string username, string password, string virtualHost,
-            string exchange, string queueName, ushort retryCount, string connectionName)
+            string exchange, string queueName, ushort retryCount, string connectionName, int port = 5672)
         {
             HostName = hostName;
             Username = username;
@@ -15,15 +19,40 @@
             QueueName = queueName;
             RetryCount = retryCount;
             ConnectionName = connectionName;
+            Port = port;
         }
 
         public string HostName { get; set; }
+
+        public int Port { get; set; } = 5672;
+        
         public string Username { get; set; }
+        
         public string Password { get; set; }
+        
         public string VirtualHost { get; set; }
+        
         public string Exchange { get; set; }
+        
         public string QueueName { get; set; }
+        
         public string ConnectionName { get; set; }
+        
         public ushort RetryCount { get; set; }
+
+        public string GetConnectionString()
+        {
+            var sb = new StringBuilder("amqp://");
+
+            if (!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password)) 
+                sb.Append($"{Username}:{Password}@");
+
+            sb.Append($"{HostName}:{Port}/");
+            
+            if (!string.IsNullOrEmpty(VirtualHost) && VirtualHost[0] == '/') 
+                sb.Append($"{VirtualHost}");
+
+            return sb.ToString();
+        }
     }
 }
